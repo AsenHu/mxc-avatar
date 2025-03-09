c85db55855ed00e090d50080012adb42ca728031
AsenHu <78863300+AsenHu@users.noreply.github.com>
2025-03-09 17:35:31 +0000

    支持了个规范里已经弃用的接口避免其他服务端有奇妙问题

---
+++ functions/_matrix/media/v3/download/[[DEPRECATED]].ts
@@ -0,0 +1,41 @@
+import { generateFromString } from 'lib/gen_avatar'
+
+async function onRequestGet(context: { request: Request }) {
+    // 获取信息
+    const url = new URL(context.request.url);
+    const path = url.pathname.split('/');
+    const mediaId = path[5];
+    const fileName = path[6];
+
+    let disposition = "attachment";
+    // 检查 fileName 是否存在
+    if (fileName) {
+        disposition = `attachment; filename="${fileName}"`;
+    }
+
+    // 生成头像
+    const svg = await generateFromString(mediaId);
+
+    // 返回 SVG
+    return new Response(svg, {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'image/svg+xml',
+            'Content-Disposition': disposition,
+        },
+    });
+}
+
+async function onRequestOptions() {
+    return new Response(null, {
+        status: 204,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Max-Age': '86400',
+        }
+    });
+}
+
+export { onRequestGet, onRequestOptions }
